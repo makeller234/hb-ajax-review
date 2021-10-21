5a598f8092eb5b62139a68f1af934f6c30a1ec64
Melissa Keller <melissa.keller23@gmail.com>
2021-10-21 16:44:46 -0500

    finished ajax queries/functions in ajax-review

--- static/ajax-review.js
+++ static/ajax-review.js
@@ -2,6 +2,10 @@
 
 function showFortune(evt) {
   // TODO: get the fortune and show it in the fortune-text div
+  evt.preventDefault();
+  $.get('/fortune', resp => {
+    $('#fortune-text').html(resp);
+  });
 }
 
 $('#get-fortune-button').on('click', showFortune);
@@ -12,6 +16,10 @@ function showWeather(evt) {
   evt.preventDefault();
   const url = `/weather?zipcode=${$('#zipcode-field').val()}`;
   // TODO: request weather with that URL and show the forecast in #weather-info
+
+  $.get(url, resp =>{
+    $('#weather-info').html(resp.forecast);
+  });
 }
 
 $('#weather-form').on('submit', showWeather);
